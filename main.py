c6f255bded738ef99e99ef8dd2b445b59f470aa9
Nyks <redgo3666@gmail.com>
2025-05-21 15:23:30 +0800

    Initialized a function where it calls the class and store

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ def display_menu():
     print("2. Take Quiz")
     print("3. Exit")
     print("----------------------------")
+
+def main():
+    quiz_filename = "quiz.txt"
+    quiz_creator = QuizCreator(quiz_filename)
+    quiz_game = QuizGame(quiz_filename)
