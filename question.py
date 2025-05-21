4d98527b7764a0ef0c6cfa640ca4d05457596999
Nyks <redgo3666@gmail.com>
2025-05-21 15:08:08 +0800

    Entered spaces for better readability

--- question.py
+++ question.py
@@ -3,14 +3,18 @@ class Questions:
         self.question_text = question_text
         self.choices = choices
         self.correct_answer_key = correct_answer_key
+
     def display(self):
         print(f"\nQuestion: {self.question_text}")
         for letter, choice in self.choices.items():
             print(f"{letter}. {choice}")
+
     def is_correct(self, user_answer_key):
         return user_answer_key.lower() == self.correct_answer_key
+    
     def get_correct_answer_text(self):
         return self.choices.get(self.correct_answer_key, "N/A")
+    
     def to_file_format(self):
         output = f"Question: {self.question_text}\n"
         for letter, choice in self.choices.items():
--- quiz_create.py
+++ quiz_create.py
@@ -4,6 +4,7 @@ from question import Questions
 class QuizCreator:
     def __init__(self, filename="quiz.txt"):
         self.filename = filename
+
     def create_quiz_item(self):
         question_input = input("Enter your question or type 'exit' to finish: ")
         if question_input.lower() == "exit":
@@ -21,9 +22,11 @@ class QuizCreator:
             correct_answer_key = input("Enter the correct answer using (a,b,c, or d): ").lower()
 
         return Questions(question_input, answers, correct_answer_key)
+    
     def write_quiz_to_file(self, question_obj):
         with open(self.filename, "a") as file:
             file.write(question_obj.to_file_format())
+
     def create_new_quiz(self):
         if os.path.exists(self.filename):
             os.remove(self.filename)
