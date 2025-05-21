0706c7cb843fa23ba1ceeda3a5223f92f46039f7
Nyks <redgo3666@gmail.com>
2025-05-21 15:17:15 +0800

    Add a function the reads the quiz txtfile and extract question, answers, and correct answers. that appened to a new question object.

--- quiz_game.py
+++ quiz_game.py
@@ -6,3 +6,20 @@ class QuizGame:
         self.filename = filename
         self.questions = []
         self.score = 0
+
+    def load_questions_from_file(self):
+        self.questions = []
+        with open(self.filename, "r") as file:
+            while True:
+                question_line = file.readline().strip()
+                if not question_line:
+                    break
+                question_text = question_line.removeprefix("Question: ")
+                choices = {}
+                for i in range(4):
+                    choice_line = file.readline().strip()
+                    letter, answer = choice_line.split(".", 1)
+                    choices[letter.strip()] = answer.strip()
+                correct_answer_line = file.readline().strip()
+                correct_answer_key = correct_answer_line.removeprefix("correct answer: ")
+                self.questions.append(Questions(question_text, choices, correct_answer_key))
