5122aa4c86f9060c9cd09f0410773afd879df77f
Nyks <redgo3666@gmail.com>
2025-05-21 14:48:12 +0800

    Add a function that the user's answer matches the correct answer

--- question.py
+++ question.py
@@ -1,9 +1,11 @@
 class Questions:
-     def __init__(self, question_text, choices, correct_answer_key):
+    def __init__(self, question_text, choices, correct_answer_key):
         self.question_text = question_text
         self.choices = choices
         self.correct_answer_key = correct_answer_key
-     def display(self):
+    def display(self):
         print(f"\nQuestion: {self.question_text}")
         for letter, choice in self.choices.items():
             print(f"{letter}. {choice}")
+    def is_correct(self, user_answer_key):
+        return user_answer_key.lower() == self.correct_answer_key
