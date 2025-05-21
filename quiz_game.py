c8d1cecee416356f8d338f7c653d91cae5a6cd03
Nyks <redgo3666@gmail.com>
2025-05-21 15:01:13 +0800

    Initialized QuizGame

--- quiz_game.py
+++ quiz_game.py
@@ -1,2 +1,8 @@
 import random
 from question import Questions
+
+class QuizGame:
+    def __init__(self, filename="quiz.txt"):
+        self.filename = filename
+        self.questions = []
+        self.score = 0
