e2aedf37e156b97129bcea349eec747c9e719be7
Nyks <redgo3666@gmail.com>
2025-05-21 14:50:26 +0800

    Add a function that converts the question object to a string format suitable for writing to a file

--- question.py
+++ question.py
@@ -11,3 +11,9 @@ class Questions:
         return user_answer_key.lower() == self.correct_answer_key
     def get_correct_answer_text(self):
         return self.choices.get(self.correct_answer_key, "N/A")
+    def to_file_format(self):
+        output = f"Question: {self.question_text}\n"
+        for letter, choice in self.choices.items():
+            output += f"{letter}. {choice}\n"
+        output += f"correct answer: {self.correct_answer_key}\n"
+        return output
