5b7988cb59ebedd2d8a0f0f124ca583fb9db20c1
Nyks <redgo3666@gmail.com>
2025-05-21 14:55:47 +0800

    Add a function that prompts the user to input details for a single quiz question and return to questions

--- quiz_create.py
+++ quiz_create.py
@@ -4,3 +4,20 @@ from question import Questions
 class QuizCreator:
     def __init__(self, filename="quiz.txt"):
         self.filename = filename
+    def create_quiz_item(self):
+        question_input = input("Enter your question or type 'exit' to finish: ")
+        if question_input.lower() == "exit":
+            return None
+
+        answers = {}
+        answers['a'] = input("Enter answer a: ")
+        answers['b'] = input("Enter answer b: ")
+        answers['c'] = input("Enter answer c: ")
+        answers['d'] = input("Enter answer d: ")
+
+        correct_answer_key = input("Enter the correct answer using (a,b,c, or d): ").lower()
+        while correct_answer_key not in answers:
+            print("Invalid input! Enter the correct answer (a,b,c, or d)")
+            correct_answer_key = input("Enter the correct answer using (a,b,c, or d): ").lower()
+
+        return Questions(question_input, answers, correct_answer_key)
