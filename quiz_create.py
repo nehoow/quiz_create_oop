789766993e255440978c108c9d9a23bbdd0e6443
Nyks <redgo3666@gmail.com>
2025-05-21 14:56:51 +0800

    Add a function that Writes a single Question object to the quiz file.

--- quiz_create.py
+++ quiz_create.py
@@ -21,3 +21,6 @@ class QuizCreator:
             correct_answer_key = input("Enter the correct answer using (a,b,c, or d): ").lower()
 
         return Questions(question_input, answers, correct_answer_key)
+    def write_quiz_to_file(self, question_obj):
+        with open(self.filename, "a") as file:
+            file.write(question_obj.to_file_format())
