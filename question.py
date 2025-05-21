24d8ec3ef38460240a22133fe2517d0003b0340a
Nyks <redgo3666@gmail.com>
2025-05-21 14:45:32 +0800

    Initialized question object

---
+++ question.py
@@ -0,0 +1,5 @@
+class Questions:
+     def __init__(self, question_text, choices, correct_answer_key):
+        self.question_text = question_text
+        self.choices = choices
+        self.correct_answer_key = correct_answer_key
