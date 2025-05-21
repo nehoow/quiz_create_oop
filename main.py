210e756a8f031524ed2669d47c8285ee25bd8c7a
Nyks <redgo3666@gmail.com>
2025-05-21 15:22:36 +0800

    Add a function that display a text menu to the user

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from quiz_create import QuizCreator
-from quiz_game import QuizGame  
+from quiz_game import QuizGame
+
+def display_menu():
+    """Displays the main menu options to the user."""
+    print("\n--- Quiz Application Menu ---")
+    print("1. Create New Quiz")
+    print("2. Take Quiz")
+    print("3. Exit")
+    print("----------------------------")
